dabc5737c68ddccdeacc898e01511158d8f63c94
Jude M Battista <jbattista20@my.whitworth.edu>
2019-03-19 14:04:26 -0700

    Finish initial instruction set
    Update test code
    Generate new output file
    Add comments

---
+++ input.asm
@@ -0,0 +1,19 @@
+.data
+# Change the arrays to be variable length
+error bit 1
+list dword 1 2 3 4 5 6 7
+
+.code
+# Labels are on their own line starting with a colon, and only use the first word
+:start <- This is the start
+mov $d0 $zer        # mov zero into the first data register
+jmp loaddata        # skip to the load data section
+movi $ipt 1234      # change the instruction pointer to 1234. This should never execute
+:loaddata           # load data from memory
+lw $d1 list         # copy the first element from list into $d1
+lw $d1 list[4]      # copy the fifth element from list into $d1
+lw $d2 0x34578      # copy the element in memory location 0x34578 into $d2
+:addloop            # add some stuff       
+add $d0 $d1         # add $d1 to $d0 and store in $d1
+beq addloop         # if the result of the prior calculation was 0, loop
+addi $d0 15         # add 15 to $d0 and store it in $d0
--- parser.py
+++ parser.py
@@ -1,15 +1,20 @@
+import re
+
+# Information holds the opcode and argument lengths for every instruction
 information = {}
 information["movi"] =   ('10000000', 5, 16)
 information["mov"]  =   ('00000000', 5,  5)
 information["add"]  =   ('00000001', 5,  5)
 information["addi"] =   ('10000001', 5, 16)
 information["ladd"] =   ('00001000', 5,  5)
-information["beq"]  =   ('01001011', 24, 0)
+information["beq"]  =   ('01000000', 24, 0)
 information["jmp"]  =   ('00100000', 24, 0)
 information["loop"] =   ('00010000', 19, 5)
 information["lw"]   =   ('10000100', 5, 16)
 information["sw"]   =   ('10000101', 5, 16)
 
+# Registers maps the english name for the register to the binary value representing the
+# register internally
 registers = {
     "$spt" : bin(0)[2:].zfill(5),
     "$ipt" : bin(1)[2:].zfill(5),
@@ -42,12 +47,15 @@ registers = {
     "$flg" : bin(27)[2:].zfill(5)
     }
 
+# Data will hold the name and information of all our variables
 data = {}
+# lines holds the contents of the assembler code file
 lines = []
+# Translate variable types into number of bits
 data_size_identifiers = { "dword" : 32, "word" : 16, "byte" : 8, "nibble" : 4, "bit" : 1}
 
 # Get all the code without the comments
-with open("input.txt") as f:
+with open("input.asm") as f:
     for line in f:
         line = line.partition('#')[0].strip()
         if line != '':
@@ -55,7 +63,7 @@ with open("input.txt") as f:
 
 binary_tracker = ''
 
-# If there is a data section
+# If there is a data section, consume it and convert it to hex
 if lines[0] == ".data":
     del lines[0]
     while lines[0] != ".code":
@@ -67,8 +75,9 @@ if lines[0] == ".data":
         offset_from_start = len(binary_tracker) + 32 # maybe don't calculate this every time?
         for x in var_elements:
             binary_tracker += format(int(x), '0%db' % data_size_identifiers[var_size])
-        data[var_name] = (var_size, offset_from_start)
-    
+        data[var_name] = (data_size_identifiers[var_size], offset_from_start)
+
+# contains the lines to be written to the machine code file 
 hex_output = []
 
 total_chars = ((len(binary_tracker) // 32) + 1) * 32
@@ -85,41 +94,44 @@ while binary_tracker != '':
 # Add the jump instruction at the start of our code
 def jump(offset_from_current):
     opcode = information["jmp"][0]
-    arg = bin(offset_from_current)[2:].ljust(24, '0')
+    arg = bin(offset_from_current)[2:].zfill(24)
     return hex(int(opcode + arg, 2))[2:]
 
 tbj = jump(len(hex_output) + 1)
 hex_output.insert(0, tbj)
 
+# start assembling the code
 if lines[0] == '.code':
     del lines[0]
 else:
     print("NO CODE SECTION!")
 
+# Walk through the code and consume all the labels, adding them to the label dictionary
 # Labels are addressed by their name, and we return the offset from the start of the code section
 labels = {}
-
 count = len(hex_output)
-
-for line in lines:
-    next_hex = ''
+for i, line in enumerate(lines):
     # Labels are on their own line starting with a colon, and only use the first word
     if line[0] == ':':   
         label = line.split(' ')[0][1:]
         labels[label] = count
+        del lines[i]
         continue
-
     count += 1
 
-    # Process the current instruction
+for line in lines:
+    next_hex = ''
 
+    # Process the current instruction
     things = line.split()
     instr = things[0]
     args = things[1:]
     opcode = information[instr][0]
 
+    # if we know about the instruction...
     if instr in information.keys():
         current_bin = ''
+        # giant case statement 
         if instr in ['mov', 'add']:
             current_bin = opcode + registers[args[0]] + registers[args[1]]
         elif instr in ['movi','addi']:
@@ -136,15 +148,31 @@ for line in lines:
                 arg2_bin = bin(int(arg2))[2:].zfill(19)
             # We are dealing with a variable
             else:
-                if '[' in arg2 and ']' in arg2:
-                    pass
-                else:
+                matches = re.split('[\[ \]]', arg2)
+                if len(matches) > 1: # we have a variable with an index
+                    variable = matches[0]
+                    offset = int(matches[1]) * data[variable][0] + data[variable][1]
+                    arg2_bin = str(bin(offset)[2:].zfill(19))
+                else: # plain old variable
                     arg2_bin = str(bin(data[arg2][1])[2:].zfill(19))
             current_bin = opcode + registers[args[0]] + arg2_bin
+        elif instr in ['jmp', 'beq']:
+            jLoc = args[0]
+            # if we're jumping to a label
+            if jLoc in labels.keys():
+                print(jLoc)
+                jLoc = (labels[jLoc] - len(hex_output)) * 4
+                print(jLoc)
+            else: # otherwise we're jumping a set number of instructions
+                jLoc = int(jLoc) * 4
+            if jLoc < 0:
+                jLoc = (2 ** 24) + jLoc
+            arg = bin(jLoc)[2:].zfill(24)
+            print(arg)
+            current_bin = opcode + arg
         if current_bin != '':
             next_hex = hex(int(current_bin.ljust(32, '0'), 2))[2:].zfill(8)
             hex_output.append(next_hex.upper())
-            
     else:
         print("The function " + instr + "does not exist!")
 
