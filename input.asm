d93dadbd3fa30a487b77aa4e5f126c365cdd3f9f
Robert C Rutherford <rrutherford20@my.whitworth.edu>
2019-05-02 18:50:59 -0700

    Added the simple input code

--- input.asm
+++ input.asm
@@ -1,19 +1,23 @@
 .data
-# Change the arrays to be variable length
-error dword 999999999
-list dword 1 2 3 4 5 6 7
-
+list1 dword 1 2 3 4
+list3 dword 5 6 8 7
 .code
-# Labels are on their own line starting with a colon, and only use the first word
-:start <- This is the start
-mov $d0 $d1        # mov zero into the first data register
-jmp loaddata        # skip to the load data section
-movi $d2 1234      # change the instruction pointer to 1234. This should never execute
-:loaddata           # load data from memory
-lw $d1 list         # copy the first element from list into $d1
-lw $d1 list[4]      # copy the fifth element from list into $d1
-lw $d2 0x34578      # copy the element in memory location 0x34578 into $d2
-:addloop            # add some stuff       
-add $d0 $d1         # add $d1 to $d0 and store in $d1
-beq addloop         # if the result of the prior calculation was 0, loop
-addi $d0 15         # add 15 to $d0 and store it in $d0
+:start
+movi $d0 -1			#
+movi $d1 1			#
+add $d0 $d1			#
+beq no_lists_here	#
+:lists_here			
+lload $l0 list1		#
+lload $l1 list3		#
+ladd $l0 $l1		#
+lstore $l1 list3	#
+jmp end				#
+:no_lists_here		
+lw $d3 list1[2]		#
+mov $d2 $d3			#
+add  $d3 $d2		#
+sw $d3 list3[2]		#
+jmp lists_here		#
+:end				
+jmp start			#
