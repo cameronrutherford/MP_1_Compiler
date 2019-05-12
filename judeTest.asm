a8f2d632820f5aa55e574c27745a04ac1079c82b
Jude M Battista <jbattista20@my.whitworth.edu>
2019-05-12 13:31:56 -0700

    VGA test. Now with loops

--- judeTest.asm
+++ judeTest.asm
@@ -8,7 +8,40 @@ list3 dword 5 6 7 8		# list of dwords
 .code
 lload $l1 list1
 lload $l2 listdown
+lload $l3 listup
 :startloop
+
+
+mov $d2 $d0
+addi $d2 50
+:downloop
 ladd $l1 $l2
 vgastore $l1
-jmp startloop
+mov $d1 $d0
+addi $d1 1000
+:stalldown
+addi $d1 -1
+beq continuedown
+jmp stalldown
+:continuedown
+addi $d2 -1
+beq startuploop
+jmp downloop
+
+
+:startuploop
+mov $d2 $d0
+addi $d2 50
+:uploop
+ladd $l1 $l3
+vgastore $l1
+mov $d1 $d0
+addi $d1 1000
+:stallup
+addi $d1 -1
+beq continueup
+jmp stallup
+:continueup
+addi $d2 -1
+beq startloop
+jmp uploop
