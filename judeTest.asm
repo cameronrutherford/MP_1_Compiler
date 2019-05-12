556e86befff9b93498d8086a2ab99ebb15dbaf65
Jude M Battista <jbattista20@my.whitworth.edu>
2019-05-12 14:03:07 -0700

    Move further

--- judeTest.asm
+++ judeTest.asm
@@ -12,7 +12,7 @@ lload $l3 listup
 :startloop
 
 mov $d2 $d0
-addi $d2 50
+addi $d2 1000
 :downloop
 ladd $l1 $l2
 vgastore $l1
@@ -30,12 +30,12 @@ jmp downloop
 
 :startuploop
 mov $d2 $d0
-addi $d2 50
+addi $d2 1000
 :uploop
 ladd $l1 $l3
 vgastore $l1
 mov $d1 $d0
-addi $d1 1000
+addi $d1 100000
 :stallup
 addi $d1 -1
 beq continueup
