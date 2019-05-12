22285c1b8cc175061499b2efed823e18a21038b7
Jude M Battista <jbattista20@my.whitworth.edu>
2019-05-12 12:27:07 -0700

    VGA test program

--- judeTest.asm
+++ judeTest.asm
@@ -1,6 +1,14 @@
 .data
 list1 dword 1 2 3 4		# list of dwords
+listup dword -1 0 0 0	# move up
+listdown dword 1 0 0 0	# move down
+listleft dword 0 -1 0 0	# move left
+listright dword 0 1 0 0	# move right
 list3 dword 5 6 7 8		# list of dwords
 .code
-:start
+lload $l1 list1
+lload $l2 listdown
+:startloop
+ladd $l1 $l2
 vgastore $l1
+jmp startloop
